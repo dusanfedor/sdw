418574c2bdc5740586f5103cedac85e2a9245245
dusanfedor <dfedor@post.cz>
2020-12-15 18:51:08 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+import FirstPage from "./pages/FirstPage/FirstPage";
+import SecondPage from "./pages/SecondPage/SecondPage";
+
+function App() {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Redirect to="/second" />
+      </Route>
+      <Route exact path="/first" component={FirstPage} />
+      <Route exact path="/second" component={SecondPage} />
+    </Switch>
+  );
+}
+
+export default App;
---
+++ src/pages/SecondPage/SecondPage.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import "../pages.css";
+
+function SecondPage() {
+  const [myColor, setMyColor] = useState("black");
+
+  const setRandomColor = () => {
+    const randomColor = "#" + (((1 << 24) * Math.random()) | 0).toString(16);
+    setMyColor(randomColor);
+  };
+
+  return (
+    <div className="body" style={{ color: myColor }}>
+      <h1>"Bezradnost a nespokojenost jsou prvním předpoklady k pokroku"</h1>
+      <button onClick={setRandomColor}>Change Color</button>
+      <div className="container">
+        <div className="row">
+          <nav className="col-md-auto navbar">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="/second">
+                  HOME
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="https://doprava-oprava.jednoduse.cz/"
+                >
+                  Stehovani
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="https://angel-wings19.webnode.cz">
+                  Whisky
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="https://doprava-oprava.cz/?preview_id=233&preview_nonce=21ef8c292f&_thumbnail_id=-1&preview=true"
+                >
+                  Green Planet
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="https://stehovaci-prace9.webnode.cz"
+                >
+                  Stehovaci prace
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="col contentText">
+            React spatřil světlo světa v květnu 2013. Opensourcoval ho Facebook,
+            který ho už několik let před tím sám interně používal a vylepšoval.
+            Prvotní vydání se však dočkalo velkého výsměchu. Odezva byla tak
+            špatná, že Facebook chvíli uvažoval i o jeho stáhnutí. Terčem
+            kritiky se stalo především míchání "HTML a programování". Podobné
+            obavy nedávno vyjádřili i někteří prominentní čeští webaři. Postupně
+            se však ukázalo, že došlo k pouze nepochopení základního konceptu a
+            nejen FE vývojáři si začli rychle osvojovat a užívat nové
+            fundamenty, které React přinesl.
+          </div>
+        </div>
+        <h2>
+          <a href="https://angel-wings19.webnode.cz/">Whisky</a>
+        </h2>
+        <h3>
+          <a href="https://doprava-a-oprava-sro.business.site/?utm_source=gmb&utm_medium=referral">
+            MY Business
+          </a>
+        </h3>
+        <h4>
+          <a href="https://doprava-oprava.cz/?preview_id=233&preview_nonce=21ef8c292f&_thumbnail_id=-1&preview=true">
+            GreenPlanet
+          </a>
+        </h4>
+        <h5>
+          <a href="/second">Home</a>
+        </h5>
+        <h6>
+          <a href="https://stehovaci-prace9.webnode.cz/">Stehovaci prace</a>
+        </h6>
+      </div>
+      <h1>COPYRIGHT ©company</h1>
+    </div>
+  );
+}
+
+export default SecondPage;
