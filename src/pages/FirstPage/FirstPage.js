484fb83eb128ff8523a3dd57b648c6219666d652
dusanfedor <dfedor@post.cz>
2020-12-23 18:18:25 +0100

    adding flags

--- src/pages/FirstPage/FirstPage.js
+++ src/pages/FirstPage/FirstPage.js
@@ -1,14 +1,20 @@
 import React from "react";
 import slovakia from "../../img/slovakia_img.jpg";
+import czechia from "../../img/czechia_img.jpg";
 import "../pages.css";
 
 function FirstPage() {
   return (
     <div className="body">
-      <div className="firstPageTitle">
+      <div className="flags">
         <div className="flag">
           <a href="https://doprava-oprava.jednoduse.cz/">
             <img alt="SVK" src={slovakia} />
+          </a>
+        </div>
+        <div className="flag">
+          <a href="https://doprava-oprava.jednoduse.cz/">
+            <img alt="CZECH" src={czechia} />
           </a>
         </div>
       </div>
