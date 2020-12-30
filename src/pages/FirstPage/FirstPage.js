4b11e63faec961f2bf962d0ad21043186bab1a03
dusanfedor <dfedor@post.cz>
2020-12-30 20:14:11 +0100

    modifying first page and CSS

--- src/pages/FirstPage/FirstPage.js
+++ src/pages/FirstPage/FirstPage.js
@@ -6,11 +6,23 @@ import game from "../../img/game_img.jpg";
 import avakin from "../../img/avakin_img.jpg";
 import roblox from "../../img/roblox_img.jpg";
 import youtube from "../../img/youtube_img.jpg";
+import zepeto from "../../img/zepeto_img.jpg";
+import shazam from "../../img/shazam_img.jpg";
+import amongus from "../../img/amongus_img.jpg";
 
 import "../pages.css";
 
 function FirstPage() {
   const [jurko, setJurko] = useState(null);
+
+  /*let d = new Date();
+  if (d.getHours() > 5 && d.getHours() < 18) document.write("Dobrý den</br>");
+  else document.write("Dobrý večer</br>");
+
+  for (let i = 3; i < 5; i++) {
+    document.write("bubu bu<br />");
+  }
+  document.write("Viki!");*/
 
   const writeLang = (language) => {
     setJurko(language);
@@ -30,7 +42,6 @@ function FirstPage() {
             </a>
           </button>
         </div>
-
         <div className="flag">
           <button onClick={() => writeLang("Tic")}>
             <a
@@ -42,7 +53,6 @@ function FirstPage() {
             </a>
           </button>
         </div>
-
         <div className="flag">
           <button onClick={() => writeLang("Game")}>
             <a
@@ -54,7 +64,6 @@ function FirstPage() {
             </a>
           </button>
         </div>
-
         <div className="flag">
           <button onClick={() => writeLang("Avakin")}>
             <a
@@ -66,9 +75,8 @@ function FirstPage() {
             </a>
           </button>
         </div>
-
         <div className="flag">
-          <button onClick={() => writeLang("Roblox")}>
+          <button onClick={() => writeLang("Avakin")}>
             <a
               target="_blank"
               rel="noopener noreferrer"
@@ -78,7 +86,6 @@ function FirstPage() {
             </a>
           </button>
         </div>
-
         <div className="flag">
           <button onClick={() => writeLang("Youtube")}>
             <a
@@ -90,7 +97,39 @@ function FirstPage() {
             </a>
           </button>
         </div>
-
+        <div className="flag">
+          <button onClick={() => writeLang("Avakin")}>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://studio.zepeto.me/console/auth/signin?redirect=/console"
+            >
+              <img alt="zepeto" src={zepeto} />
+            </a>
+          </button>
+        </div>
+        <div className="flag">
+          <button onClick={() => writeLang("Avakin")}>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.shazam.com/cs/myshazam"
+            >
+              <img alt="shazam" src={shazam} />
+            </a>
+          </button>
+        </div>
+        <div className="flag">
+          <button onClick={() => writeLang("Amongus")}>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="http://www.innersloth.com/index.php"
+            >
+              <img alt="amongus" src={amongus} />
+            </a>
+          </button>
+        </div>
         <div className="result" hidden={jurko ? false : true}>
           {jurko}
         </div>
