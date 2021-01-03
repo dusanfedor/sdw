32f18bf108c4968104501a3f4ee257530f8e2079
dusanfedor <dfedor@post.cz>
2021-01-03 21:53:06 +0100

    preparing basic visuals for calculator

--- src/pages/FirstPage/FirstPage.js
+++ src/pages/FirstPage/FirstPage.js
@@ -5,7 +5,49 @@ import "../pages.css";
 function FirstPage() {
   return (
     <div className="body2">
-      This is the first page of a blank application ready to be created.
+      <div className="container">
+        <div class="row">
+          <div class="col-10">display</div>
+        </div>
+
+        <div class="row">
+          <div class="col-2">←</div>
+          <div class="col-2">CE</div>
+          <div class="col-2">C</div>
+          <div class="col-2">±</div>
+          <div class="col-2">√</div>
+        </div>
+
+        <div class="row">
+          <div class="col-2">7</div>
+          <div class="col-2">8</div>
+          <div class="col-2">9</div>
+          <div class="col-2">/</div>
+          <div class="col-2">%</div>
+        </div>
+
+        <div class="row">
+          <div class="col-2">4</div>
+          <div class="col-2">5</div>
+          <div class="col-2">6</div>
+          <div class="col-2">*</div>
+          <div class="col-2">1/x</div>
+        </div>
+
+        <div class="row">
+          <div class="col-2">1</div>
+          <div class="col-2">2</div>
+          <div class="col-2">3</div>
+          <div class="col-2">+</div>
+          <div class="col-2">-</div>
+        </div>
+
+        <div class="row">
+          <div class="col-4">0</div>
+          <div class="col-2">.</div>
+          <div class="col-4">=</div>
+        </div>
+      </div>
     </div>
   );
 }
