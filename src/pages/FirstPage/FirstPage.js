6d842560114e16a7f6a0ed308077081f4fb5ffca
dusanfedor <dfedor@post.cz>
2021-01-04 20:37:06 +0100

    finishing design of calculator

--- src/pages/FirstPage/FirstPage.js
+++ src/pages/FirstPage/FirstPage.js
@@ -6,47 +6,49 @@ function FirstPage() {
   return (
     <div className="body2">
       <div className="container">
-        <div class="row">
-          <div class="col-10">display</div>
-        </div>
+        <table>
+          <div class="row">
+            <div class="col-10">display</div>
+          </div>
 
-        <div class="row">
-          <div class="col-2">←</div>
-          <div class="col-2">CE</div>
-          <div class="col-2">C</div>
-          <div class="col-2">±</div>
-          <div class="col-2">√</div>
-        </div>
+          <div class="row">
+            <div class="col-2">←</div>
+            <div class="col-2">CE</div>
+            <div class="col-2">C</div>
+            <div class="col-2">±</div>
+            <div class="col-2">√</div>
+          </div>
 
-        <div class="row">
-          <div class="col-2">7</div>
-          <div class="col-2">8</div>
-          <div class="col-2">9</div>
-          <div class="col-2">/</div>
-          <div class="col-2">%</div>
-        </div>
+          <div class="row">
+            <div class="col-2">7</div>
+            <div class="col-2">8</div>
+            <div class="col-2">9</div>
+            <div class="col-2">/</div>
+            <div class="col-2">%</div>
+          </div>
 
-        <div class="row">
-          <div class="col-2">4</div>
-          <div class="col-2">5</div>
-          <div class="col-2">6</div>
-          <div class="col-2">*</div>
-          <div class="col-2">1/x</div>
-        </div>
+          <div class="row">
+            <div class="col-2">4</div>
+            <div class="col-2">5</div>
+            <div class="col-2">6</div>
+            <div class="col-2">*</div>
+            <div class="col-2">1/x</div>
+          </div>
 
-        <div class="row">
-          <div class="col-2">1</div>
-          <div class="col-2">2</div>
-          <div class="col-2">3</div>
-          <div class="col-2">+</div>
-          <div class="col-2">-</div>
-        </div>
+          <div class="row">
+            <div class="col-2">1</div>
+            <div class="col-2">2</div>
+            <div class="col-2">3</div>
+            <div class="col-2">+</div>
+            <div class="col-2">-</div>
+          </div>
 
-        <div class="row">
-          <div class="col-4">0</div>
-          <div class="col-2">.</div>
-          <div class="col-4">=</div>
-        </div>
+          <div class="row">
+            <div class="col-4">0</div>
+            <div class="col-2">.</div>
+            <div class="col-4">=</div>
+          </div>
+        </table>
       </div>
     </div>
   );
