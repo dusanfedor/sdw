8111091bc4e11a469b8aa9fe9aa81c763574631f
dusanfedor <dfedor@post.cz>
2021-01-11 15:37:54 +0100

    adding more functions related to display

--- src/pages/FirstPage/FirstPage.js
+++ src/pages/FirstPage/FirstPage.js
@@ -6,16 +6,31 @@ function FirstPage() {
   const [display, setDisplay] = useState("0");
 
   function displayInput(input) {
-    setDisplay(display + input);
+    // TODO 1: ak pises viac znamienok po sebe, tak sa zobrazi vzdy len to posledne, na ktore kliknes
+    // TODO 2: nula nemoze byt na zaciatku cisla (ak to nie je len 0 alebo v ramci desatinneho cisla)
+    // https://www.itnetwork.cz/javascript/zaklady/javascript-js-tutorial-datove-typy-retezce-cisla-funkce
+    // https://www.itnetwork.cz/javascript/zaklady/javascript-tutorial-podminky
+    // https://www.itnetwork.cz/javascript/zaklady/javascript-tutorial-cykly-for-while
+    // https://www.itnetwork.cz/javascript/zaklady/javascript-tutorial-zaciname-s-javascriptem-podminky-cykly-pole-svatky
+    let result = display + input;
+
+    if (result == "00") {
+      result = "0";
+    }
+
+    setDisplay(result);
   }
 
   function clear() {
     setDisplay("0");
   }
 
+  function evaluate() {
+    setDisplay("0"); // TODO: prerobit na vypocet prikladu
+  }
+
   function add(a, b) {
     const result = a + b;
-    alert(result);
   }
 
   function subtract(a, b) {
@@ -50,7 +65,7 @@ function FirstPage() {
 
   function getFraction(a) {
     const result = 1 / a;
-    alert(result);
+    setDisplay(result);
   }
 
   return (
@@ -82,16 +97,24 @@ function FirstPage() {
 
         <div className="row">
           <div className="col-2">
-            <button className="calcBtn">7</button>
+            <button className="calcBtn" onClick={() => displayInput("7")}>
+              7
+            </button>
           </div>
           <div className="col-2">
-            <button className="calcBtn">8</button>
+            <button className="calcBtn" onClick={() => displayInput("8")}>
+              8
+            </button>
           </div>
           <div className="col-2">
-            <button className="calcBtn">9</button>
+            <button className="calcBtn" onClick={() => displayInput("9")}>
+              9
+            </button>
           </div>
           <div className="col-2">
-            <button className="calcBtn">/</button>
+            <button className="calcBtn" onClick={() => displayInput(" / ")}>
+              /
+            </button>
           </div>
           <div className="col-2">
             <button className="calcBtn">x²</button>
@@ -100,16 +123,24 @@ function FirstPage() {
 
         <div className="row">
           <div className="col-2">
-            <button className="calcBtn">4</button>
+            <button className="calcBtn" onClick={() => displayInput("4")}>
+              4
+            </button>
           </div>
           <div className="col-2">
-            <button className="calcBtn">5</button>
+            <button className="calcBtn" onClick={() => displayInput("5")}>
+              5
+            </button>
           </div>
           <div className="col-2">
-            <button className="calcBtn">6</button>
+            <button className="calcBtn" onClick={() => displayInput("6")}>
+              6
+            </button>
           </div>
           <div className="col-2">
-            <button className="calcBtn">*</button>
+            <button className="calcBtn" onClick={() => displayInput(" * ")}>
+              *
+            </button>
           </div>
           <div className="col-2">
             <button className="calcBtn">1/x</button>
@@ -118,19 +149,29 @@ function FirstPage() {
 
         <div className="row">
           <div className="col-2">
-            <button className="calcBtn">1</button>
+            <button className="calcBtn" onClick={() => displayInput("1")}>
+              1
+            </button>
           </div>
           <div className="col-2">
-            <button className="calcBtn">2</button>
+            <button className="calcBtn" onClick={() => displayInput("2")}>
+              2
+            </button>
           </div>
           <div className="col-2">
-            <button className="calcBtn">3</button>
+            <button className="calcBtn" onClick={() => displayInput("3")}>
+              3
+            </button>
           </div>
           <div className="col-2">
-            <button className="calcBtn">+</button>
+            <button className="calcBtn" onClick={() => displayInput(" + ")}>
+              +
+            </button>
           </div>
           <div className="col-2">
-            <button className="calcBtn">-</button>
+            <button className="calcBtn" onClick={() => displayInput(" - ")}>
+              -
+            </button>
           </div>
         </div>
 
@@ -141,10 +182,14 @@ function FirstPage() {
             </button>
           </div>
           <div className="col-2">
-            <button className="calcBtn">.</button>
+            <button className="calcBtn" onClick={() => displayInput(".")}>
+              .
+            </button>
           </div>
           <div className="col-4">
-            <button className="calcBtn">=</button>
+            <button className="calcBtn" onClick={evaluate}>
+              =
+            </button>
           </div>
         </div>
       </div>
